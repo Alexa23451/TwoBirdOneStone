bce0341ef67115cadda33a38a35947483ff04b3a
MasterQQ <aqquan2017@gmail.com>
2021-10-11 09:49:08 +0700

    Add processor editor

---
+++ Assets/Editor/MyAllPostprocessor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEditor;
+
+public class MyAllPostprocessor : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string str in importedAssets)
+        {
+            Debug.Log("IMPORT Asset: " + str);
+        }
+        foreach (string str in deletedAssets)
+        {
+            Debug.Log("Deleted Asset: " + str);
+        }
+
+        for (int i = 0; i < movedAssets.Length; i++)
+        {
+            Debug.Log("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
+        }
+    }
+}
