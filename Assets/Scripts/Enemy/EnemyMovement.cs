ac5e47611c0cd146fc9165784092e96e27454c0f
MasterQQ <aqquan2017@gmail.com>
2021-10-14 09:31:06 +0700

    Enemy movement

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private float _leftLimit, _rightLimit;
+    [SerializeField] private float _leftLimitRange, _rightLimitRange;
+
+    [SerializeField] private float _speed;
+
+    [SerializeField] [Range(0, 0.8f)] private float _leftOffsetPercent;
+    [SerializeField] [Range(0, 0.8f)] private float _rightOffsetPercent;
+
+    public bool moveRightFirst;
+
+    private void Awake()
+    {
+        var height = Camera.main.orthographicSize;
+        var ratio = (float)Screen.width / Screen.height;
+        var width = ratio * height;
+
+        _leftLimit = -width;
+        _rightLimit = width;
+    }
+
+    void Start()
+    {
+        _leftLimitRange = _leftLimit * _leftOffsetPercent;
+        _rightLimitRange = _rightLimit * _rightOffsetPercent;
+    }
+
+    void Update()
+    {
+        if (moveRightFirst)
+        {
+            if(transform.localScale.x == -1f)
+            {
+                transform.localScale = new Vector3(1,1,1);
+            }
+
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+        else
+        {
+            if (transform.localScale.x == 1f)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+            transform.position -= Vector3.right * Time.deltaTime * _speed;
+        }
+
+        //range check
+        if(transform.position.x > _rightLimitRange)
+        {
+            moveRightFirst = false;
+        }
+        
+        if(transform.position.x < _leftLimitRange)
+        {
+            moveRightFirst = true;
+        }
+    }
+}
