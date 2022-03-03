0ab57da6f2b1e63d93074772ceeaf4531d82a461
MasterQQ <aqquan2017@gmail.com>
2022-03-03 23:33:45 +0700

    Add findref2, remove unuse asset, change to rider editor

--- Assets/Scripts/Base/Core/DragAndDrop.cs
+++ Assets/Scripts/Base/Core/DragAndDrop.cs
@@ -2,32 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragAndDrop : MonoBehaviour
+namespace Quan_Utility
 {
-    private bool isDragging = false;
-    private Camera cam;
-
-    private void Start()
+    public class DragAndDrop : MonoBehaviour
     {
-        cam = Camera.main;
-    }
+        private bool isDragging = false;
+        private Camera cam;
 
-    public void OnMouseDown()
-    {
-        isDragging = true;
-    }
-
-    public void OnMouseUp()
-    {
-        isDragging = false;
-    }
-
-    void Update()
-    {
-        if (isDragging)
+        private void Start()
         {
-            Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            transform.Translate(mousePosition);
+            cam = Camera.main;
+        }
+
+        public void OnMouseDown()
+        {
+            isDragging = true;
+        }
+
+        public void OnMouseUp()
+        {
+            isDragging = false;
+        }
+
+        void Update()
+        {
+            if (isDragging)
+            {
+                Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+                transform.Translate(mousePosition);
+            }
         }
     }
 }
